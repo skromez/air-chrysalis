c0c81fd9c2bccb5d43818364446b9d2f1da137b2
Dmitry Novikov <kromez12@gmail.com>
2022-05-07 21:31:17 +0200

    WIP

--- front-end/src/shared/api.ts
+++ front-end/src/shared/api.ts
@@ -1,5 +1,5 @@
 export const api = {
-  sequence: 'http://localhost:8080/https://api.sequence.app/rpc/API',
-  indexer: 'http://localhost:8080/https://polygon-indexer.sequence.app/rpc/Indexer',
-  metadata: 'http://localhost:8080/https://metadata.sequence.app/rpc/Metadata/'
+  sequence: 'https://api.sequence.app/rpc/API',
+  indexer: 'https://polygon-indexer.sequence.app/rpc/Indexer',
+  metadata: 'https://metadata.sequence.app/rpc/Metadata'
 }
