adb46b9aeb4fe37a1b33e1360eabc7ade76d7f73
Dmitry Novikov <kromez12@gmail.com>
2022-05-13 10:11:23 +0200

    WIP

--- front-end/src/services/indexer.service.ts
+++ front-end/src/services/indexer.service.ts
@@ -10,8 +10,8 @@ export class IndexerService {
     })
     const {data: {balances}} = await this.axiosInstance.post('/GetTokenBalances', {
       accountAddress,
-      contractAddress: contractBalances[0].contractAddress
-      // contractAddress: '0x631998e91476da5b870d741192fc5cbc55f5a52e'
+      // contractAddress: contractBalances[0].contractAddress
+      contractAddress: '0x631998e91476da5b870d741192fc5cbc55f5a52e'
       // contractAddress: '0xd045df0b4d618eb228087a40049338d3747e5542'
     })
     return {tokenIds: balances.map((bal) => bal.tokenID), contractAddress: contractBalances[0].contractAddress }
