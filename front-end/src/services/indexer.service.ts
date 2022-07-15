6febedf785b022ec386d679f5a5b5c99782e3f8f
Dmitrii Novikov <kromez12@gmail.com>
2022-07-15 21:51:44 +0200

    fix CORS error

--- front-end/src/services/indexer.service.ts
+++ front-end/src/services/indexer.service.ts
@@ -2,15 +2,20 @@ import axios from 'axios';
 import { skyWeaverAddress } from '../shared/contract';
 
 export class IndexerService {
-  private static readonly axiosInstance = axios.create({baseURL: 'https://polygon-indexer.sequence.app/rpc/Indexer'})
-
-  static async getTokenIDs(accountAddress: string): Promise<{tokenId: string, amount: number}[]> {
-    const {data: {balances}} = await this.axiosInstance.post('/GetTokenBalances', {
-      accountAddress,
-      contractAddress: skyWeaverAddress
-    })
-    return balances
-      .filter(({contractAddress}) => contractAddress == skyWeaverAddress)
-      .map((bal) => ({tokenId: String(bal.tokenID), amount: Number(bal.balance) / 100}))
-  }
+	static async getTokenIDs(accountAddress: string): Promise<{ tokenId: string; amount: number }[]> {
+		const res = await fetch('https://polygon-indexer.sequence.app/rpc/Indexer/GetTokenBalances', {
+			method: 'POST',
+			body: JSON.stringify({
+				accountAddress,
+				contractAddress: skyWeaverAddress
+			}),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+		const { balances } = await res.json();
+		return balances
+			.filter(({ contractAddress }) => contractAddress == skyWeaverAddress)
+			.map((bal) => ({ tokenId: String(bal.tokenID), amount: Number(bal.balance) / 100 }));
+	}
 }
