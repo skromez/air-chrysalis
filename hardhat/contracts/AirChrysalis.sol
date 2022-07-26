2ea34e2fc2c6e9203c7c9570f21ea905254eb43a
Dmitrii Novikov <kromez12@gmail.com>
2022-07-26 19:03:41 +0200

    add multiple winners implementation, fix tests

--- hardhat/contracts/AirChrysalis.sol
+++ hardhat/contracts/AirChrysalis.sol
@@ -11,9 +11,8 @@ contract AirChrysalis is VRFConsumerBaseV2 {
     VRFCoordinatorV2Interface COORDINATOR;
     uint64 immutable s_subscriptionId;
     bytes32 immutable s_keyHash;
-    uint32 immutable s_callbackGasLimit = 40000;
+    uint32 immutable s_callbackGasLimit = 70000;
     uint16 immutable s_requestConfirmations = 3;
-    uint32 immutable s_numWords = 2;
 
     uint256[] public s_randomWords;
     uint256 public s_requestId;
@@ -23,22 +22,27 @@ contract AirChrysalis is VRFConsumerBaseV2 {
         uint256[2][] tokensTuple;
         address[] participants;
         bool finished;
-        address winner;
+        uint32 winnersAmount;
         address owner;
         uint256 requestId;
+        uint256[] randomWords;
     }
-    Giveaway[] giveaways;
 
-    mapping (address => uint256[]) addressGiveaways;
+    struct RequestToGiveaway {
+        address addr;
+        uint256 giveawayId;
+    }
+
+    mapping (address => Giveaway[]) addressToGiveaways;
     mapping (uint256 => mapping(address => bool)) giveawayParticipants;
-    mapping (uint256 => uint256) requestToGiveaway;
+    mapping (uint256 => RequestToGiveaway) requestToGiveaway;
 
     event GiveawayCreated(address indexed account, uint256 giveawayId);
     event GiveawayEntered(address indexed account, address indexed participant, uint256 giveawayId);
     event GiveawayCanceled(address indexed account, uint256 giveawayId);
 
     event RandomizingGiveawayWinner(uint256 indexed requestId, address indexed account, uint256 indexed giveawayId);
-    event GiveawayWinnerVerified(address indexed winnerAddress, uint256 indexed giveawayId);
+    event GiveawayWinnersVerified(uint256[] randomWords, uint256 indexed giveawayId);
 
     constructor(
         uint64 subscriptionId,
@@ -50,63 +54,58 @@ contract AirChrysalis is VRFConsumerBaseV2 {
         s_subscriptionId = subscriptionId;
     }
 
-    function createGiveaway(address _contractAddr, uint256[2][] memory _tokensTuple) public {
-        address[] memory participants;
-        giveaways.push(Giveaway(_contractAddr, _tokensTuple, participants, false, address(0), msg.sender, 0));
-        addressGiveaways[msg.sender].push(giveaways.length);
-        emit GiveawayCreated(msg.sender, giveaways.length);
+    function createGiveaway(address _contractAddr, uint256[2][] memory _tokensTuple, uint32 winnersAmount) public {
+        if (winnersAmount > 1) {
+            require(_tokensTuple.length % winnersAmount == 0, "amount of items should be divided to winners amount without remainder");
+        }
+        addressToGiveaways[msg.sender].push(Giveaway(_contractAddr, _tokensTuple, new address[](0), false, winnersAmount, msg.sender, 0, new uint256[](0)));
+        emit GiveawayCreated(msg.sender, addressToGiveaways[msg.sender].length);
     }
 
-    function getAccountGiveaways(address account) public view returns(uint256[] memory) {
-        return addressGiveaways[account];
-    }
-
-    function getGiveaway(uint256 giveawayId) public view returns(Giveaway memory) {
-        return giveaways[giveawayId];
+    function getAccountGiveaways(address account) public view returns(Giveaway[] memory) {
+        return addressToGiveaways[account];
     }
 
     function isParticipatingInGiveaway(address account, uint256 _giveawayId) public view returns(bool) {
         return giveawayParticipants[_giveawayId][account];
     }
 
-    function enterGiveaway(uint256 giveawayId) public {
-        require(giveawayId + 1 <= giveaways.length, "giveaway with specified index doesn't exist");
-        Giveaway memory giveaway = giveaways[giveawayId];
+    function enterGiveaway(address addr, uint256 giveawayId) public {
+        require(giveawayId + 1 <= addressToGiveaways[addr].length, "giveaway with specified index doesn't exist");
+        Giveaway memory giveaway = addressToGiveaways[addr][giveawayId];
         require(giveaway.owner != msg.sender, "owner of giveaway can't participate");
         require(giveaway.finished == false, "giveaway ended");
         require(giveawayParticipants[giveawayId][msg.sender] == false, "can't enroll twice to the same giveaway");
         giveawayParticipants[giveawayId][msg.sender] = true;
-        giveaways[giveawayId].participants.push(msg.sender);
+        addressToGiveaways[addr][giveawayId].participants.push(msg.sender);
         emit GiveawayEntered(giveaway.owner, msg.sender, giveawayId);
     }
 
-    function finishGiveaway(uint256 giveawayId) public {
-        Giveaway storage giveaway = giveaways[giveawayId];
+    function finishGiveaway(address addr, uint256 giveawayId) public {
+        Giveaway storage giveaway = addressToGiveaways[addr][giveawayId];
         require(msg.sender == giveaway.owner, "only host of giveaway can finish it");
         if (giveaway.participants.length > 0) {
             s_requestId = COORDINATOR.requestRandomWords(
                 s_keyHash,
                 s_subscriptionId,
                 s_requestConfirmations,
-                s_callbackGasLimit,
-                s_numWords
+                s_callbackGasLimit * giveaway.winnersAmount,
+                giveaway.winnersAmount
             );
-            requestToGiveaway[s_requestId] = giveawayId;
+            requestToGiveaway[s_requestId] = RequestToGiveaway(addr, giveawayId);
             giveaway.requestId = s_requestId;
             emit RandomizingGiveawayWinner(s_requestId, giveaway.owner, giveawayId);
         } else {
-            giveaway.winner = address(0);
             giveaway.finished = true;
             emit GiveawayCanceled(giveaway.owner, giveawayId);
         }
     }
 
     function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
-        uint256 giveawayId = requestToGiveaway[requestId];
-        Giveaway storage giveaway = giveaways[giveawayId];
-        uint256 winnerIndex = (randomWords[0] % giveaway.participants.length);
-        giveaway.winner = giveaway.participants[winnerIndex];
-        giveaway.finished = true;
-        emit GiveawayWinnerVerified(giveaway.winner, giveawayId);
+        RequestToGiveaway memory giveawayInfo = requestToGiveaway[requestId];
+
+        addressToGiveaways[giveawayInfo.addr][giveawayInfo.giveawayId].finished = true;
+        addressToGiveaways[giveawayInfo.addr][giveawayInfo.giveawayId].randomWords = randomWords;
+        emit GiveawayWinnersVerified(randomWords, giveawayInfo.giveawayId);
     }
 }
--- hardhat/test/AirChrysalis.js
+++ hardhat/test/AirChrysalis.js
@@ -2,38 +2,63 @@ const { assert, expect } = require("chai")
 const { deployments, ethers } = require("hardhat")
 
 describe("AirChrysalis Unit Tests", async function () {
-  let airChrysalis, vrfCoordinatorV2Mock, nftContract, deployer, addr1, addr2;
+  let airChrysalis, vrfCoordinatorV2Mock, nftContract, deployer, addr1, addr2, addr3, addr4,addr5,addr6,addr7,addr8,addr9,addr10;
 
   beforeEach(async () => {
-    [deployer, addr1, addr2] = await ethers.getSigners();
+    [deployer, addr1, addr2, addr3, addr4,addr5,addr6,addr7,addr8,addr9,addr10] = await ethers.getSigners();
     await deployments.fixture(["mocks", "vrf"]);
     nftContract = await ethers.getContract("NftContractMock");
     airChrysalis = await ethers.getContract("AirChrysalis");
     vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock");
   });
 
-  it("Should successfully create giveaway", async () => {
-    await expect(airChrysalis.createGiveaway(nftContract.address, [[1, 100]])).to.emit(
+  it("Should fail creating giveaway: can't split items between winners evenly #1", async () => {
+    await expect(airChrysalis.createGiveaway(nftContract.address, [[1, 100]], 2))
+      .to.be.revertedWith('amount of items should be divided to winners amount without remainder');
+  });
+
+  it("Should fail creating giveaway: can't split items between winners evenly #2", async () => {
+    await expect(airChrysalis.createGiveaway(nftContract.address, [[1, 100], [2, 100], [3, 100], [4, 100]], 3))
+      .to.be.revertedWith('amount of items should be divided to winners amount without remainder');
+  });
+
+
+  it("Should successfully create giveaway and be able to split items evenly #1", async () => {
+    await expect(airChrysalis.createGiveaway(nftContract.address, [[1, 100], [2, 100], [3, 100], [4, 100]], 2)).to.emit(
+      airChrysalis,
+      "GiveawayCreated"
+    ).withArgs(deployer.address, 1);
+  });
+
+  it("Should successfully create giveaway and be able to split items evenly #2", async () => {
+    await expect(airChrysalis.createGiveaway(nftContract.address, [[1, 100], [2, 100], [3, 100], [4, 100]], 4)).to.emit(
+      airChrysalis,
+      "GiveawayCreated"
+    ).withArgs(deployer.address, 1);
+  });
+
+  it("Should successfully create giveaway for 1 winner", async () => {
+    await expect(airChrysalis.createGiveaway(nftContract.address, [[1, 100], [2, 100], [3, 100], [4, 100]], 1)).to.emit(
         airChrysalis,
       "GiveawayCreated"
     ).withArgs(deployer.address, 1);
   });
 
-  it("Should successfully return correct giveaways list for given account", async () => {
-    await airChrysalis.createGiveaway(nftContract.address, [[1, 100]])
-    let giveawaysList = await airChrysalis.getAccountGiveaways(deployer.address)
-    assert.equal(giveawaysList.length, 1, 'length should be 1')
-    assert.equal(giveawaysList[0].toNumber(), 1, 'giveawayId should be 1')
+  it("Should successfully return correct giveaways for given account", async () => {
+    await airChrysalis.createGiveaway(nftContract.address, [[1, 100]], 1)
+    const giveaways = await airChrysalis.getAccountGiveaways(deployer.address)
+    assert.equal(giveaways.length, 1, 'length should be 1')
   });
 
   it("Should successfully return giveaway by given giveawayId", async () => {
-    await airChrysalis.createGiveaway(nftContract.address, [[1, 100]])
-    const giveaway = await airChrysalis.getGiveaway(0)
-    assert.containsAllKeys(giveaway, ['contractAddr', 'tokensTuple', 'participants', 'finished', 'winner', 'owner', 'requestId'], 'should contain all Giveaway struct fields')
+    await airChrysalis.createGiveaway(nftContract.address, [[1, 100], [2, 100]], 2)
+    const [giveaway] = await airChrysalis.getAccountGiveaways(deployer.address)
+    assert.containsAllKeys(giveaway, ['contractAddr', 'tokensTuple', 'participants', 'finished', 'winnersAmount', 'owner', 'requestId', 'randomWords'], 'should contain all Giveaway struct fields')
     assert.equal(giveaway.contractAddr, nftContract.address, 'contract address should be equal to nft address')
     assert.equal(giveaway.finished, false, 'giveaway should not be finished')
     assert.equal(giveaway.owner, deployer.address, 'owner address should equal deployer address')
-    assert.equal(giveaway.winner, ethers.constants.AddressZero, 'winner should be empty');
+    assert.equal(giveaway.winnersAmount, 2, 'winners amount should be 1');
+    assert.equal(giveaway.randomWords.length, 0, 'randomWords array should be empty');
     assert.equal(giveaway.requestId, 0, 'requestId should be 0');
     assert.equal(giveaway.participants.length, 0, 'participants should be empty');
     assert.equal(giveaway.tokensTuple[0][0].toNumber(), 1, 'tokenId should be equal to 1');
@@ -41,74 +66,74 @@ describe("AirChrysalis Unit Tests", async function () {
   });
 
   it("Should not let to enter giveaway which doesn't exist", async () => {
-    await expect(airChrysalis.connect(addr1).enterGiveaway(0))
+    await expect(airChrysalis.connect(addr1).enterGiveaway(deployer.address, 0))
       .to.be.revertedWith("giveaway with specified index doesn't exist");
   });
 
   it("Should not let owner of giveaway to participate in it", async () => {
-      await airChrysalis.createGiveaway(nftContract.address, [[1, 100]])
-      await expect(airChrysalis.enterGiveaway(0))
+      await airChrysalis.createGiveaway(nftContract.address, [[1, 100]], 1)
+      await expect(airChrysalis.enterGiveaway(deployer.address, 0))
           .to.be.revertedWith("owner of giveaway can't participate");
   });
 
   it("Should not let to enter giveaway if it's finished", async () => {
-    await airChrysalis.createGiveaway(nftContract.address, [[1, 100]]);
-    await airChrysalis.connect(addr1).enterGiveaway(0);
-    await airChrysalis.finishGiveaway(0);
+    await airChrysalis.createGiveaway(nftContract.address, [[1, 100]], 1);
+    await airChrysalis.connect(addr1).enterGiveaway(deployer.address, 0);
+    await airChrysalis.finishGiveaway(deployer.address, 0);
     const requestId = await airChrysalis.s_requestId()
     await vrfCoordinatorV2Mock.fulfillRandomWords(requestId, airChrysalis.address)
-    await expect(airChrysalis.connect(addr2).enterGiveaway(0))
+    await expect(airChrysalis.connect(addr2).enterGiveaway(deployer.address, 0))
       .to.revertedWith('giveaway ended');
   });
 
   it("Should not let to enter giveaway twice", async () => {
-    await airChrysalis.createGiveaway(nftContract.address, [[1, 100]]);
-    await airChrysalis.connect(addr1).enterGiveaway(0);
-    await expect(airChrysalis.connect(addr1).enterGiveaway(0))
+    await airChrysalis.createGiveaway(nftContract.address, [[1, 100]], 1);
+    await airChrysalis.connect(addr1).enterGiveaway(deployer.address, 0);
+    await expect(airChrysalis.connect(addr1).enterGiveaway(deployer.address, 0))
       .to.be.revertedWith("can't enroll twice to the same giveaway");
   });
 
   it("Should let to enter giveaway which exist", async () => {
-    await airChrysalis.createGiveaway(nftContract.address, [[1, 100]]);
-    await expect(airChrysalis.connect(addr1).enterGiveaway(0)).to.emit(
+    await airChrysalis.createGiveaway(nftContract.address, [[1, 100]], 1);
+    await expect(airChrysalis.connect(addr1).enterGiveaway(deployer.address, 0)).to.emit(
       airChrysalis,
       "GiveawayEntered"
     ).withArgs(deployer.address, addr1.address, 0);
   });
 
   it("Should be able to cancel giveaway", async () => {
-    await airChrysalis.createGiveaway(nftContract.address, [[1, 100]]);
-    await expect(airChrysalis.finishGiveaway(0))
+    await airChrysalis.createGiveaway(nftContract.address, [[1, 100]], 1);
+    await expect(airChrysalis.finishGiveaway(deployer.address, 0))
       .to.emit(airChrysalis, 'GiveawayCanceled').withArgs(deployer.address, 0)
-    const giveaway = await airChrysalis.getGiveaway(0)
-    assert.equal(giveaway.winner, ethers.constants.AddressZero, 'winner should be zero address');
+    const [giveaway] = await airChrysalis.getAccountGiveaways(deployer.address)
+    assert.equal(giveaway.randomWords.length, 0, `giveaway's random numbers should be empty`);
     assert.equal(giveaway.finished, true, 'giveaway should be finished');
   });
 
   it("Should be able to finish giveaway", async () => {
-    await airChrysalis.createGiveaway(nftContract.address, [[1, 100]]);
-    await airChrysalis.connect(addr1).enterGiveaway(0);
-    await expect(airChrysalis.finishGiveaway(0))
+    await airChrysalis.createGiveaway(nftContract.address, [[1, 100]], 1);
+    await airChrysalis.connect(addr1).enterGiveaway(deployer.address, 0);
+    await expect(airChrysalis.finishGiveaway(deployer.address, 0))
         .to.emit(airChrysalis, 'RandomizingGiveawayWinner')
     const requestId = await airChrysalis.s_requestId()
     await expect(
         vrfCoordinatorV2Mock.fulfillRandomWords(requestId, airChrysalis.address)
-    ).to.emit(airChrysalis, "GiveawayWinnerVerified")
-    const giveaway = await airChrysalis.getGiveaway(0)
-    assert.equal(giveaway.winner, addr1.address, 'winner should be equal to addr1');
+    ).to.emit(airChrysalis, "GiveawayWinnersVerified")
+    const [giveaway] = await airChrysalis.getAccountGiveaways(deployer.address)
+    assert.equal(giveaway.randomWords.length, 1, 'random numbers should have 1 number');
     assert.equal(giveaway.finished, true, 'giveaway should be finished');
   });
 
   it("Should not be able to finish giveaway if called by not host", async () => {
-      await airChrysalis.createGiveaway(nftContract.address, [[1, 100]]);
-      await expect(airChrysalis.connect(addr1).finishGiveaway(0)).to.be.revertedWith('only host of giveaway can finish it');
+      await airChrysalis.createGiveaway(nftContract.address, [[1, 100]], 1);
+      await expect(airChrysalis.connect(addr1).finishGiveaway(deployer.address, 0)).to.be.revertedWith('only host of giveaway can finish it');
   });
 
   it("Should successfully return boolean if account participating or not", async () => {
-      await airChrysalis.createGiveaway(nftContract.address, [[1, 100]]);
+      await airChrysalis.createGiveaway(nftContract.address, [[1, 100]], 1);
       let result = await airChrysalis.connect(addr1).isParticipatingInGiveaway(addr1.address, 0);
       assert.equal(result, false, 'account is not participating in the giveaway')
-      await airChrysalis.connect(addr1).enterGiveaway(0);
+      await airChrysalis.connect(addr1).enterGiveaway(deployer.address, 0);
       result = await airChrysalis.connect(addr1).isParticipatingInGiveaway(addr1.address, 0);
       assert.equal(result, true, 'account is participating in the giveaway')
   });
